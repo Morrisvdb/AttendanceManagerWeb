fc472fad88a6b86b04a356324b3469705491adcb
Morrisvdb <morrisvandenbergh@gmail.com>
2025-11-13 11:23:46 +0100

    Minor changes

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-git pull
+# git pull
 
 python3 -m venv .
 
@@ -6,5 +6,6 @@ source ./bin/activate
 
 pip install -r requirements.txt
 
-# If it wont run: export set FLASK_APP=main.app
+export set FLASK_APP=main.app
+
 flask run --host 0.0.0.0 --port 5002 --debug
